3b81348706626f2590ce992f578484ef33fe927a
Serhii <slobojko21@gmail.com>
2024-07-08 23:18:25 +0300

    dashboard layout

--- src/components/dashboard-layout/sidebar/Sidebar.tsx
+++ src/components/dashboard-layout/sidebar/Sidebar.tsx
@@ -22,10 +22,8 @@ export function Sidebar() {
 						size={38}
 					/>
 					<span className='text-2xl font-bold relative'>
-						RED Planner
-						<span className='absolute -top-1 -right-6 text-xs opacity-40 rotate-[18deg] font-normal'>
-							beta
-						</span>
+						Trello clone
+
 					</span>
 				</Link>
 				<div className='p-3 relative'>
@@ -46,7 +44,7 @@ export function Sidebar() {
 					rel='noreferrer'
 					className='hover:text-primary text-brand-300 transition-colors'
 				>
-					RED Group
+					Trello clone
 				</a>
 				. <br /> All rights reserved.
 			</footer>
